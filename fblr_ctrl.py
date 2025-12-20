156a1e4ba09ada116c888cdac14af0266a16a452
HoangPhuc <phucnguyeho@gmail.com>
2025-12-20 19:19:48 +0700

    ctrl with alert

--- fblr_ctrl.py
+++ fblr_ctrl.py
@@ -5,12 +5,12 @@ PORT = 4210
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-print("Commands: f=forward, b=backward, l=left, r=right, q=quit")
+print("Commands: f=forward, b=backward, l=left, r=right, q=quit, a=alert, o=off_alert")
 while True:
     cmd = input("Enter command: ").strip().lower()
     if cmd == "q":
         break
-    elif cmd in ["f", "b", "l", "r"]:
+    elif cmd in ["f", "b", "l", "r", "a", "o"]:
         sock.sendto(cmd.encode(), (ESP32_IP, PORT))
         print(f"Sent {cmd}")
     else:
